959e79ec7ad716d23351a1163e567a7c7fcbab3e
anyz0 <anythinks.alauddinazhary05@gmail.com>
2024-05-21 12:13:02 +0700

    add bottom navigation to mainactivity

--- app/src/main/java/com/android/blendit/ui/MainActivity.kt
+++ app/src/main/java/com/android/blendit/ui/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.android.blendit.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.android.blendit.R
+import com.android.blendit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setFullscreen()
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+//                R.id.account -> startActivity(Intent(this, AccountActivity::class.java))
+            }
+            true
+        }
+    }
+
+    private fun setFullscreen() {
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
     }
