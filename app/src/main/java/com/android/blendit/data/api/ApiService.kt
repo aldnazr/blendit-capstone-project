97da048a57720609689bddac373bee54cb2d490a
anythinks <alauddinazhary05@gmail.com>
2024-06-12 13:57:51 +0700

    change filled color to red

--- app/src/main/java/com/android/blendit/data/api/ApiService.kt
+++ app/src/main/java/com/android/blendit/data/api/ApiService.kt
@@ -1,17 +1,12 @@
 package com.android.blendit.data.api
 
-import android.os.Parcelable
 import com.android.blendit.BuildConfig
-import com.google.gson.annotations.SerializedName
-import kotlinx.parcelize.Parcelize
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
-import java.util.Date
 
 data class ImageAnalysisRequest(val imageUri: String)
 data class ImageAnalysisResponse(val faceType: String, val skinTone: String, val description: String)
@@ -19,7 +14,7 @@ data class ImageAnalysisResponse(val faceType: String, val skinTone: String, val
 data class User(
     val login: String,
     val id: Int,
-    val avatar_url: String
+    val avatarUrl: String
 )
 
 interface ApiService {
@@ -27,7 +22,6 @@ interface ApiService {
     fun analyzeImage(
         @Body request: ImageAnalysisRequest
     ): Call<ImageAnalysisResponse>
-
 
     @GET("users/{username}/followers")
     @Headers("Authorization: ${BuildConfig.TOKEN}")
